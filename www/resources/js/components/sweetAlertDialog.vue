37f02ecd49cd4fc5f01c63cbc9d9f7ddfb1cbc25
OmarHossamEldin <okandil273@gmail.com>
2021-02-26 16:40:18 +0200

    finishing-sweetAlertComponent

---
+++ www/resources/js/components/sweetAlertDialog.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-dialog
+        transition="dialog-top-transition"
+        max-width="600"
+        v-model="dialog"
+    >
+        <template v-slot:default="dialog">
+            <v-card>
+                <v-toolbar dir="rtl" :color="type" dark>
+                    <h3>
+                        {{message}}
+                        <v-icon>mdi-account-group-outline</v-icon>
+                    </h3>
+                </v-toolbar>
+                <v-card-text>
+                    <div
+                        class="text-h5 pa-2 text-right"
+                        v-for="(errorfield, index) in errors"
+                        :key="index"
+                    >
+                        <h6 v-for="(error, index) in errorfield" :key="index">
+                            {{ error }}
+                        </h6>
+                    </div>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                    <v-btn text @click="dialog.value = false">Close</v-btn>
+                </v-card-actions>
+            </v-card>
+        </template>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    name: "sweetAlertDialog",
+    props: ["dialog", "type", "message", "errors"]
+};
+</script>
+
+<style scoped></style>
